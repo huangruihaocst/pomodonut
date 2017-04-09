64a4d00372076568c2d168d64c660712ffe503d3
BenWng <wnghan@gmail.com>
2017-04-09 19:34:59 -0400

    Sensor works inside service

--- app/src/main/java/edu/neu/madcourse/zhongxiruihao/pomodonut/MyService.java
+++ app/src/main/java/edu/neu/madcourse/zhongxiruihao/pomodonut/MyService.java
@@ -1,7 +1,12 @@
 package edu.neu.madcourse.zhongxiruihao.pomodonut;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.CountDownTimer;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
@@ -12,6 +17,15 @@ import android.widget.Toast;
  */
 
 public class MyService extends Service {
+
+    private SensorManager mSensorManager;
+    private final Service thisService=this;
+    private float mAccelCurrent;
+    private float mAccelLast;
+    private float mAccel; // last acceleration including gravity
+
+
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent){
@@ -20,9 +34,15 @@ public class MyService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId){
+        mSensorManager = (SensorManager)this.getSystemService(Context.SENSOR_SERVICE);
+        mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        mAccelCurrent=SensorManager.GRAVITY_EARTH;
+        mAccelLast=SensorManager.GRAVITY_EARTH;
+        mAccel = 0.00f;
 
+        /**
         final Service s=this;
-        CountDownTimer countDownTimer=new CountDownTimer(15000,3000){
+        CountDownTimer countDownTimer=new CountDownTimer(1500000,3000){
             @Override
             public void onTick(long millisUntilFinished){
                 Toast.makeText(s,"hello",Toast.LENGTH_SHORT).show();
@@ -30,13 +50,38 @@ public class MyService extends Service {
             @Override
             public void onFinish(){}
         };
-        countDownTimer.start();
+        countDownTimer.start();*/
         return START_STICKY;
     }
 
     @Override
     public void onDestroy(){
         super.onDestroy();
+        mSensorManager.unregisterListener(mSensorListener);
         Toast.makeText(this,"Service Destroyed",Toast.LENGTH_LONG).show();
     }
+
+
+
+    private final SensorEventListener mSensorListener=new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            float x=sensorEvent.values[0];
+            float y=sensorEvent.values[1];
+            float z=sensorEvent.values[2];
+            mAccelLast = mAccelCurrent;
+            mAccelCurrent = (float) Math.sqrt((double) (x*x + y*y + z*z));
+            float delta = mAccelCurrent - mAccelLast;
+            mAccel = mAccel * 0.9f + delta;
+
+            if (mAccel > 24) {
+                Toast.makeText(thisService, "" + mAccel, Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
+    };
 }
