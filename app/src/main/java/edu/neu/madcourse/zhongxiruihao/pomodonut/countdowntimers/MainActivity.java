da702a3f79f8e232e5050913fc48f9ec8adf76ef
BenWng <wnghan@gmail.com>
2017-04-15 22:33:58 -0400

    Add three methods into MainActivity

--- app/src/main/java/edu/neu/madcourse/zhongxiruihao/pomodonut/countdowntimers/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/zhongxiruihao/pomodonut/countdowntimers/MainActivity.java
@@ -1,5 +1,8 @@
 package edu.neu.madcourse.zhongxiruihao.pomodonut.countdowntimers;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,8 +10,11 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 import edu.neu.madcourse.zhongxiruihao.pomodonut.R;
+import edu.neu.madcourse.zhongxiruihao.pomodonut.sensor.AccelProcessService;
+import edu.neu.madcourse.zhongxiruihao.pomodonut.sensor.RecordAccelService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,5 +46,39 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(new CountdownTimersPagerAdapter(getSupportFragmentManager(),
                 NUM_PAGES, fragments));
     }
+
+
+
+    private void startSensorServices(){
+        if (!isMyServiceRunning(RecordAccelService.class)) {
+            startService(new Intent(getBaseContext(), RecordAccelService.class));
+            Toast.makeText(this, "RecordAccelService started", Toast.LENGTH_LONG).show();
+        }
+
+        if (!isMyServiceRunning(AccelProcessService.class)) {
+            startService(new Intent(getBaseContext(), AccelProcessService.class));
+            Toast.makeText(this, "AccelProcess started", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private boolean isMyServiceRunning(Class<?> serviceClass){
+        ActivityManager manager=(ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service: manager.getRunningServices(Integer.MAX_VALUE)){
+            if (serviceClass.getName().equals(service.service.getClassName())){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void stopSensorServices(View view){
+        stopService(new Intent(getBaseContext(),RecordAccelService.class));
+        stopService(new Intent(getBaseContext(),AccelProcessService.class));
+    }
+
+
+
+
+
 
 }
