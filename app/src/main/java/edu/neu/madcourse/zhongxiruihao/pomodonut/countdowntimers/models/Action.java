ff5b30d00079a7ed4cf344a63a10357e581f6087
Ruihao Huang <huangruihaocst@126.com>
2017-04-22 15:34:49 -0400

    Two new class Event and Action.

---
+++ app/src/main/java/edu/neu/madcourse/zhongxiruihao/pomodonut/countdowntimers/models/Action.java
@@ -0,0 +1,23 @@
+package edu.neu.madcourse.zhongxiruihao.pomodonut.countdowntimers.models;
+
+import com.orm.SugarRecord;
+
+/**
+ * Created by huangruihao on 4/22/17.
+ */
+
+public class Action extends SugarRecord {
+
+    public Event event;
+
+    public long startTime;
+    public long endTime;
+
+    public Action() {}
+
+    public Action(Event event) {
+        this.event = event;
+    }
+
+
+}
--- app/src/main/java/edu/neu/madcourse/zhongxiruihao/pomodonut/countdowntimers/models/Event.java
+++ app/src/main/java/edu/neu/madcourse/zhongxiruihao/pomodonut/countdowntimers/models/Event.java
@@ -6,9 +6,24 @@ import com.orm.SugarRecord;
  * Created by huangruihao on 4/22/17.
  */
 
-public class Event extends SugarRecord{
+public class Event extends SugarRecord {
+
+    public String name;
+    // time supposed to use in millisecond
+    public long time;
+
+    // times this event happens
+    private int frequency;
 
     public Event() {}
 
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public Event(String name, long time) {
+        this.name = name;
+        this.time = time;
+    }
 
 }
